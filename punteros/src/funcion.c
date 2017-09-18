9c92457158ad6f045e18b16ae0cacf742052c297
Byron Quezada <byron.quezada@e-campus.uab.cat>
2017-09-18 15:33:13 +0200

    Añadí puntero a un arreglo

--- punteros/src/funcion.c
+++ punteros/src/funcion.c
@@ -3,22 +3,46 @@
 
 void miImpresion(void) {
 
-  int var1 = 5;
-  char var2[10];
-
-  printf("Dirección de la variable 1: %X\n", &var1);
-  printf("Dirección de la variable 2: %X\n", &var2);
-
   int *ptr = NULL;  // Buena práctica
   printf("El valor del puntero NULL es: %X\n", ptr);
   // Revisar un puntero nulo
   // if(!ptr)  // Válido si ptr es nulo
+
+  int var1 = 5;
+
+  printf("Dirección de la variable 1: %X\n", &var1);
 
   int *pe;     // Declaración de un puntero para enteros
   pe = &var1;  // Almacenar la dirección de var en el puntero
 
   printf("Dirección almacenada en el puntero: %X\n", pe);
   printf("Valor de la variable *pe: %d\n", *pe);
+
+  /* ------------------------- Puntero a un arreglo ------------------------- */
+  double balance[5] = {1000.0, 2.0, 3.4, 17.0, 50.0};
+
+  printf("Dirección de la variable 2: %X\n", &balance);
+
+  double *p;
+  p = balance;
+
+  // Imprimir cada valor de los elementos del arreglo
+  printf("Valores del arreglo usando puntero\n");
+
+  int i;
+  for(i = 0; i < 5; i++){
+    printf("*(p+%d): %f\n", i, *(p+i));
+  }
+
+  printf("Valores del arreglo usando balance como dirección\n");
+  for(i = 0; i < 5; i++){
+    printf("*(balance+%d): %f\n", i, *(balance+i));
+  }
+
+  printf("Valores del arreglo usando la forma convencional\n");
+  for(i = 0; i < 5; i++){
+    printf("balance[%d]: %f\n", i, balance[i]);
+  }
 
   return;
 }
